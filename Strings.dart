828bba8b306d12b62f82dd8d9c302e46a129c97c
Noejuro <noealexjr98@gmail.com>
2019-07-15 23:21:47 -0500

    Create Strings.dart

---
+++ Strings.dart
@@ -0,0 +1,21 @@
+void main() {
+  var cadena1 = 'Hola Mundo';
+  var cadena2 = 'Hola World';
+  var cadena3 = 'Hola \'Mundo\'';
+  
+  //Concatenar Strings
+  var string1 = 'Hola'
+    ' Mundo';
+  print(string1);
+  
+  var string2 = 'Hello';
+  var string3 = 'World';
+  print(string2 + ' ' + string3);
+  
+  var edad = 30;
+  String frase = 'Mi edad es ${edad} años';
+  print(frase);
+  
+  print('Mi saludo inicial: ${string2 + ' ' + string3}');
+  print('2+2= ${2+2}');
+}

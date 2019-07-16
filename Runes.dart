7532ab34416a62d85c4d80d483dfdac168e9139f
Noejuro <noealexjr98@gmail.com>
2019-07-16 00:15:26 -0500

    Create Runes.dart

---
+++ Runes.dart
@@ -0,0 +1,8 @@
+void main() {
+  // Runes
+  var carita = '\u{1F601}';
+  print(carita);
+  
+  Runes iconos = new Runes('\u{1F60C} \u{1F911} \u{1F92B}');
+  print(new String.fromCharCodes(iconos));
+}
